b0f811ad29fadc1c7ee2e5209839c1abc8f0cfb8
bjonescts <70379785+bjonescts@users.noreply.github.com>
2023-03-21 12:21:27 -0400

    Added checking for existing service
    Once it checks for  the running service, it will opt to update the config, then exit.

--- Install-Sysmon.ps1
+++ Install-Sysmon.ps1
@@ -9,12 +9,39 @@ The path to the working directory.  Default is user Documents.
 .EXAMPLE
 Install-Sysmon -path C:\Users\example\Desktop
 #>
+#Check for sysmon already installed
+$ServiceName = 'Sysmon64'
+$sysmon64installed = Get-Service -Name $ServiceName
 
 [CmdletBinding()]
 
 #Establish parameters for path
 param (
-    [string]$path=[Environment]::GetFolderPath("Desktop")   
+    [string]$path=$env:TEMP   
+)
+
+#Test path and create it if required
+
+if(!(test-path $path))
+{
+	Write-Information -MessageData "Path does not exist.  Creating Path..." -InformationAction Continue;
+	New-Item -ItemType Directory -Force -Path $path | Out-Null;
+	Write-Information -MessageData "...Complete" -InformationAction Continue
+}
+
+if ($sysmon64installed.Status -eq 'Running'){
+    Set-Location $path
+    Write-Host "Retrieving and Updating Configuration File..."
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml -Outfile sysmonconfig-export.xml
+    Start-Process -NoNewWindow -FilePath "$env:SystemRoot\sysmon64.exe" -ArgumentList "-c sysmonconfig-export.xml"
+    Write-Host "Configuration updated!"
+    Exit 0}
+
+[CmdletBinding()]
+
+#Establish parameters for path
+param (
+    [string]$path=$env:TEMP   
 )
 
 #Test path and create it if required
@@ -31,6 +58,13 @@ Set-Location $path
 Write-Host "Location set $path"
 
 Write-Host "Retrieving Sysmon..."
+
+$exists = test-path $path\sysmon.zip
+
+if ($exists -eq $true) {
+    Write-Host "Cleaning up old files"
+    Remove-Item $path\sysmon.zip -Force
+    Remove-Item $path\sysmon -Force -Recurse}
 
 Invoke-WebRequest -Uri https://download.sysinternals.com/files/Sysmon.zip -Outfile Sysmon.zip
 
